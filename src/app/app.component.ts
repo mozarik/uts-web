99bb15669f5e1ca59880ee7bdd99f757993160e8
mozarik <mozarikfahrozi@gmail.com>
2020-04-03 00:04:32 +0700

    Add file to github

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'Muhammad Zein';
+  panjang: Number = 10;
+  lebar: Number = 10;
+  isTeaBreakTime: Boolean = true;
+
+  handleBlur(event: any) {
+    this.title = event.target.value;
+    console.log(event.target.value);
+  }
 }
